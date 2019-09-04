533ffeb31266fb8ba5f0bf94bbcb802b3bb94c35
WhiteCastlle <cbfraphael@hotmail.com>
2019-09-03 21:14:08 -0300

    Aula 03/09

---
+++ src/com/company/numeros.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class numeros {
+    public static void main(String[] args) {
+        double A,B,C;
+        if ((A>B) && (B>C))
+        {
+            System.out.println(C+" - "+B+" - "+A);
+        }
+    }
+}
